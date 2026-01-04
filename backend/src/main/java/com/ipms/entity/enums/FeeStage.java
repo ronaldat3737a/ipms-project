83a1c9cb4563c23b2520eb8f22239c41321d1c17
ronaldat3737a <dn30894@gmail.com>
2026-01-04 22:37:09 +0700

    Test lưu db phan tammm

---
+++ backend/src/main/java/com/ipms/entity/enums/FeeStage.java
@@ -0,0 +1,7 @@
+package com.ipms.entity.enums;
+
+public enum FeeStage {
+    PHI_GD1, // Phí giai đoạn 1
+    PHI_GD2, // Phí giai đoạn 2
+    PHI_GD3  // Phí giai đoạn 3
+}
---
+++ backend/src/main/java/com/ipms/entity/enums/PaymentStatus.java
@@ -0,0 +1,7 @@
+package com.ipms.entity.enums;
+
+public enum PaymentStatus {
+    CHUA_THANH_TOAN, // Chưa thanh toán
+    CHO_XAC_NHAN,    // Chờ xác nhận
+    DA_THANH_TOAN     // Đã thanh toán
+}
